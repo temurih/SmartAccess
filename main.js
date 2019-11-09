d633a989615db4a575f03c9a8133bec2a35ee19f
Hafiz Temuri <hafiz@zept.ca>
2019-11-08 17:25:16 -0700

    adds nfc ready to scan dialog

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 function redirectToHome() {
     location.href = "home.htm";
 }
+
+function readyScanToggle() {
+    const div = document.getElementById("ready-scan");
+    const overlay = document.getElementById("overlay");
+    if (div.classList.contains("show")) {
+        div.classList.add("hide");
+        div.classList.remove("show");
+        overlay.classList.remove("overlay");
+    } else {
+        div.classList.add("show");
+        div.classList.remove("hide");
+        overlay.classList.add("overlay");
+    }
+}
